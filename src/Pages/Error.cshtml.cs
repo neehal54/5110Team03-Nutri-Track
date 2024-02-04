880b18235f371bcf70e24c4700ee8b4359ae1177
Nomi Sodnombayar <nsodnombayar@seattleu.edu>
2024-02-04 13:29:13 -0800

    t

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+///Nomi
 namespace ContosoCrafts.WebSite.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
