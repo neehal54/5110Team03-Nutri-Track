33e6e7d38e929fda7d19f1e9b3a5eb870f6e79ff
phquang2708 <phquang2708@gmail.com>
2024-02-12 19:34:50 -0800

    Add logo icon, set it in the middle of the navigator bar and change some colors on the website

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ namespace ContosoCrafts.WebSite.Pages
     /// <summary>
     /// Nomi Sodnombayar
     /// Ben Tran
-9   /// Neehal
+   /// Neehal
     /// </summary>
     public class IndexModel : PageModel
     {
