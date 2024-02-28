f0182b3cd808d4c6ce159bc47c8b4531a0ce475b
phquang2708 <phquang2708@gmail.com>
2024-02-28 02:29:20 -0800

    Added a responsive landing page, updated the background image, and adjusted the button's color when clicked

---
+++ src/Pages/Recipes.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class RecipesModel : PageModel
+    {
+        private readonly ILogger<RecipesModel> _logger;
+
+        public RecipesModel(ILogger<RecipesModel> logger) => _logger = logger;
+
+        public void OnGet()
+        {
+        }
+    }
+}
