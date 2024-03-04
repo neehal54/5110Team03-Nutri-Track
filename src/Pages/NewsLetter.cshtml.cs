aa642642bf47b5599a53a4c2da3fb194f4e5b34d
skneehal <neehal@zinzu.io>
2024-03-04 13:09:37 -0800

    Changes by Neehal

---
+++ src/Pages/NewsLetter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class NewsLetterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
